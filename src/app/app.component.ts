397f7dfed071c15684c2e0993efe522426785fdb
S3gillu <shubhamsaurabh440@yahoo.com>
2017-09-27 16:26:15 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {InventionsComponent} from './inventions/inventions.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'InventionsHub';
+
 }
